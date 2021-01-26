3c2af98a25bb3296d57722f666692079273a0212
alejandro Brand <alejandrobrand8@gmail.com>
2021-01-26 15:24:15 -0800

    server setup, still need to create the path to give the server access to the routes of the controller

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+//set static content for app from the public folder
+app.use(express.static("public"));
+
+//create req.body and parse it
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+//set handlebars.
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars",exphbs({defaultLayout:"main"}));
+app.set("view engine","handlebars");
+
+var routes = require("");
+app.use(routes);
+
+app.listen(PORT,function(){
+    console.log("server listening on: http://localhost:"+ PORT);
+});
